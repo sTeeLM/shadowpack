f8fbb9bcc2f43e95ab1e5c5b45f806f1fb7fdb3c
sTeelM <steel.mental@gmail.com>
2021-12-27 16:24:39 +0800

    pack.bat支持lang

--- pack.bat
+++ pack.bat
@@ -1,5 +1,10 @@
 mkdir Release\zip
 mkdir Release\zip\x86
+mkdir Release\zip\x86\lang
 mkdir Release\zip\x64
+mkdir Release\zip\x64\lang
+
 copy Release\Win32\bin\ShadowPack.exe Release\zip\x86
+copy Release\Win32\bin\lang\*.dll Release\zip\x86\lang
 copy Release\x64\bin\ShadowPack.exe   Release\zip\x64
+copy Release\x64\bin\lang\*.dll Release\zip\x64\lang
